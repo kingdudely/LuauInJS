b6544b50277d818cab208fad971c430845e17112
a <102840087+kingdudely@users.noreply.github.com>
2025-10-01 09:09:01 -0400

    Update index.js

--- compiler/index.js
+++ compiler/index.js
@@ -1,20 +1,15 @@
+import { applyDefaults } from "/modules/utilities/general.js";
+const textEncoder = new TextEncoder();
 const wasmLocation = import.meta.resolve('./module.wasm');
-
-class ExitStatus extends Error {
-	constructor(status) {
-		super(`Exit(${status})`);
-		this.status = status;
-	}
-};
 
 const instantiated = await WebAssembly.instantiateStreaming(fetch(wasmLocation), {
 	env: {
 		__cxa_throw: ptr => { throw new Error(`__cxa_throw (ptr=${ptr})`); },
 		__cxa_begin_catch: ptr => ptr,
-		__cxa_end_catch: () => {},
+		__cxa_end_catch: () => { },
 		__cxa_find_matching_catch_3: () => 0,
 		_abort_js: msg => { throw new WebAssembly.RuntimeError('abort: ' + msg); },
-		_emscripten_runtime_keepalive_clear: () => {},
+		_emscripten_runtime_keepalive_clear: () => { },
 		_setitimer_js: () => 0,
 		emscripten_resize_heap: (requestedSize) => {
 			const old = memory.buffer.byteLength;
@@ -23,9 +18,8 @@ const instantiated = await WebAssembly.instantiateStreaming(fetch(wasmLocation),
 
 			try {
 				memory.grow(pagesNeeded);
-				refreshViews();
 				return 1;
-			} catch (e) {
+			} catch {
 				return 0;
 			}
 		},
@@ -35,13 +29,13 @@ const instantiated = await WebAssembly.instantiateStreaming(fetch(wasmLocation),
 	},
 
 	wasi_snapshot_preview1: {
-		proc_exit: code => { throw new ExitStatus(code); }
+		proc_exit: code => { throw new WebAssembly.RuntimeError(`Return code ${code}`); } // ExitStatus
 	}
 });
 
-const { malloc, free, luau_compile, memory, __indirect_function_table  } = instantiated.instance.exports;
-
+const { malloc, free, luau_compile, memory, __indirect_function_table } = instantiated.instance.exports;
 const HEAP = {};
+
 function getHeap(arrayType) {
 	const heap = HEAP[arrayType];
 	const { buffer } = memory;
@@ -49,7 +43,6 @@ function getHeap(arrayType) {
 	return heap;
 }
 
-const textEncoder = new TextEncoder();
 function write_cstring(value) {
 	const HEAPU8 = getHeap(Uint8Array);
 
@@ -91,72 +84,76 @@ function free_array(pointer) {
 	free(pointer);
 };
 
-const DEFAULT_COMPILE_OPTIONS = [
-	["OptimizationLevel", 1],
-	["DebugLevel", 1],
-	["TypeInfoLevel", 1],
-	["CoverageLevel", 1],
-	["VectorLibName", "vector"],
-	["VectorLibConstructor", "create"],
-	["VectorType", "vector"],
-	["MutableGlobals", []],
-	["UserdataTypes", 0],
-	["LibrariesWithKnownMembers", 0],
-	["LibraryMemberTypeCb", 0],
-	["LibraryMemberConstantCb", 0],
-	["DisabledBuiltins", []],
+const OptionOrder = [ // Respective
+	"OptimizationLevel",
+	"DebugLevel",
+	"TypeInfoLevel",
+	"CoverageLevel",
+	"VectorLibName",
+	"VectorLibConstructor",
+	"VectorType",
+	"MutableGlobals",
+	"UserdataTypes",
+	"LibrariesWithKnownMembers",
+	"LibraryMemberTypeCb",
+	"LibraryMemberConstantCb",
+	"DisabledBuiltins",
 ];
+const OptionAmount = OptionOrder.length;
+// export const CompileOptions
 
-function create_luau_options(options = {}) {
-	const HEAP32 = getHeap(Int32Array);
+export default function (Source, Options) {
+	Options = applyDefaults(Options, {
+		"OptimizationLevel": 1,
+		"DebugLevel": 1,
+		"TypeInfoLevel": 1,
+		"CoverageLevel": 1,
+		"VectorLibName": "vector",
+		"VectorLibConstructor": "create",
+		"VectorType": "vector",
+		"MutableGlobals": [],
+		"UserdataTypes": 0,
+		"LibrariesWithKnownMembers": 0,
+		"LibraryMemberTypeCb": 0,
+		"LibraryMemberConstantCb": 0,
+		"DisabledBuiltins": []
+	});
 
 	const
-		optionAmount = DEFAULT_COMPILE_OPTIONS.length,
-		option_ptr = malloc(optionAmount * 4),
+		HEAPU8 = getHeap(Uint8Array),
+		HEAP32 = getHeap(Int32Array);
+
+	const
+		option_ptr = malloc(OptionAmount * 4),
 		option_base = option_ptr >> 2; // option_base_ptr
+
 	const
-		stringPointers = [option_ptr],
+		pointers = [option_ptr],
 		arrayPointers = [];
 
-	for (let i = 0; i < optionAmount; i++) {
-		const [optionName, defaultOptionValue] = DEFAULT_COMPILE_OPTIONS[i];
-		let optionValue = options[optionName] ?? defaultOptionValue;
+	for (let i = 0; i < OptionAmount; i++) {
+		const optionName = OptionOrder[i];
+		let optionValue = Options[optionName];
 
 		switch (optionValue?.constructor) {
-			case String: optionValue = stringPointers[stringPointers.length] = write_cstring(optionValue); break;
+			// What about number
+			case String: optionValue = pointers[pointers.length] = write_cstring(optionValue); break;
 			case Array: optionValue = arrayPointers[arrayPointers.length] = write_cstrings(optionValue); break;
 		};
 
 		HEAP32[option_base + i] = optionValue;
 	};
 
-	const free_options = () => {
-		stringPointers.forEach(free);
-		arrayPointers.forEach(free_array);
-	};
-
-	return [option_ptr, free_options];
-};
-
-export default function (source, options) {
-	const
-		HEAPU8 = getHeap(Uint8Array),
-		HEAP32 = getHeap(Int32Array);
-
-	const [option_ptr, free_options] = create_luau_options(options);
-
-	const src_ptr = write_cstring(source);
-	const bc_size_ptr = malloc(8);
+	const src_ptr = pointers[pointers.length] = write_cstring(Source);
+	const bc_size_ptr = pointers[pointers.length] = malloc(8);
 
 	const
-		bytecode_ptr = luau_compile(src_ptr, source.length, option_ptr, bc_size_ptr),
+		bytecode_ptr = pointers[pointers.length] = luau_compile(src_ptr, Source.length, option_ptr, bc_size_ptr),
 		bytecode_size = HEAP32[bc_size_ptr >> 2],
 		bytecode = HEAPU8.slice(bytecode_ptr, bytecode_ptr + bytecode_size);
 
-	free_options();
-	free(src_ptr);
-	free(bc_size_ptr);
-	free(bytecode_ptr);
+	pointers.forEach(free);
+	arrayPointers.forEach(free_array);
 
-	return bytecode;
-};
+	return bytecode.buffer;
+}
